7d67f4a82b2b701dc2935b5dba013c5d4b59e24f
Abdallah Deeb <abdallah@deeb.me>
2014-05-27 10:56:20 +0300

    admin commands enabled

--- xamppfire.py
+++ xamppfire.py
@@ -30,10 +30,21 @@ class JBot(ClientXMPP):
                                         wait=True)
         
     def xmpp_incoming(self, msg):
-        if msg['mucnick'] != self.nick and self.nick in msg['body']:
-            self.to_campfire(msg)
-        if 'die' in msg['body']:
-            self.leave()
+        at_nick = '@%s' % self.nick
+        if msg['mucnick'] != self.nick and at_nick.upper() in msg['body'].upper():
+            if '+die' in msg['body']:
+                self.leave()
+            elif '+who' in msg['body']:
+                self.get_campfire_users()
+            elif '+recent' in msg['body']:
+                self.get_campfire_recent()
+            elif '+reload' in msg['body']:
+                self.reconnect()
+            elif '+help' in msg['body']:
+                self.help_message()
+            else:
+                msg['body'] = msg['body'].replace(at_nick, '', 1)
+                self.to_campfire(msg)
 
     def leave(self):
         self.disconnect(wait=True)
@@ -57,17 +68,39 @@ class JBot(ClientXMPP):
             msg = "-- %s CHANGED TOPIC TO '%s'" % (username, message.body)
             
         if username!=CAMPFIRE_NICKNAME:
-            self.send_message(mto=JABBER_ROOM,
-                              mbody=msg,
-                              mtype='groupchat')
+            self.to_xmpp(msg)
 
+    def to_xmpp(self, message):
+        self.send_message(mto=JABBER_ROOM,
+                          mbody=message,
+                          mtype='groupchat')
 
     def to_campfire(self, message):
         msg = message['body']
         sender = message['mucnick']
         self.campfire_room.speak('[%s]: %s' % (sender, msg))
 
+    def get_campfire_users(self):
+        users = self.campfire_room.get_users()
+        usernames = [u['name'] for u in users]
+        msg = "The following people are currently in the chatroom: %s" % ', '.join(usernames)
+        self.to_xmpp(msg)
 
+    def get_campfire_recent(self, limit=None):
+        messages = self.campfire_room.recent(limit=limit)
+        for msg in messages:
+            self.from_campfire(msg)
+
+    def reconnect(self):
+        super(JBot, self).reconnect()
+        self.campfire_room.leave()
+        self.campfire_room.join()
+
+    def help_message(self):
+        msg = "* Send a message to the Campfire room by preceding it with @%s\n" % JABBER_NICKNAME
+        msg += "* @%s +who to list users currently in the Campfire room\n" % JABBER_NICKNAME
+        msg += "* @%s +recent to list recent messages from the Campfire room\n" % JABBER_NICKNAME
+        self.to_xmpp(msg)
 
 def error(e, room):
     print("Stream STOPPED due to ERROR: %s" % e)
